0d18f5741aaee65a71079a75a9e0e4595166be9c
Christian Hjalmarsson <christian.hjalmarsson@gmail.com>
2012-06-15 15:57:56 +0200

    Added data attribute to the twitterbutton

--- lib/so_cial/helper.rb
+++ lib/so_cial/helper.rb
@@ -2,8 +2,9 @@
 module SoCial
   module Helper
     def social_button_for(service, opts = {})
+      data = opts[:data]
       html = []
-      html << link_to('Tweet', 'https://twitter.com/share', class: 'twitter-share-button')
+      html << link_to('Tweet', 'https://twitter.com/share', class: 'twitter-share-button', data: data)
       html << '<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>'
       raw html.join("\n")
     end
